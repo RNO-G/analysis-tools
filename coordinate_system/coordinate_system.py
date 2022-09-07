234f152a300389fa52f616bc9e50eb4351a6fa14
Christoph <christophwelling@uchicago.edu>
2022-09-07 14:12:38 -0500

    Add docstrings

--- coordinate_system/coordinate_system.py
+++ coordinate_system/coordinate_system.py
@@ -5,11 +5,24 @@ import json
 
 
 class CoordinateSystem:
+    """
+    Class to convert between different coordinate systems. Not that the standard coordinate system
+    used by RNO-G is centered around the DISC hole.
+    """
     def __init__(
             self,
             origin='DISC',
             year=2022
     ):
+        """
+        Initialize the class
+
+        :param origin: string
+            Name of the reference point of the coordinate system. Should be DISC by default
+        :param year: integer
+            Year in which the reference was mapped. As the glacier and the buildings at
+            Summit move, this may change from year to year.
+        """
         filename = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             'coordinate_origins.json'
@@ -24,6 +37,22 @@ class CoordinateSystem:
             height=3260.,
             deg=True
     ):
+        """
+        Convert from geodetic coordinates (latitude, longitude, elevation)
+        to ENU coordinates (east, north, up)
+
+        :param latitude: float
+            Latitude of the point
+        :param longitude: float
+            Longitude of the point
+        :param height: float
+            Height above the reference ellipsoid of the point
+        :param deg: boolean
+            If True, parameters are assumed to be in degrees, otherwise they are
+            assumed to be in radians
+        :return: array of float
+            East, north and up coordinates relative to the coordinate system's reference point
+        """
         origin = np.copy(self.__origin)
         if not deg:
             origin[:2] *= np.pi / 180.
@@ -46,6 +75,21 @@ class CoordinateSystem:
             height=0.,
             deg=True
     ):
+        """
+        Convert from ENU coordinates (east, north, up) to
+        geodetic coordinates (latitude, longitude, elevation)
+
+        :param easting: float
+            East, relative to the coordinate system's reference point
+        :param northing: float
+            North, relative to the coordinate system's reference point
+        :param height: float
+            Up, relative to the coordinate system's reference point
+        :param deg: boolean
+            If True, geodetic coordinates will be returned in degrees, otherwise in radians
+        :return: Array of floats with shape (3,)
+            Latitude, longitude and elevation coordinate
+        """
         origin = np.copy(self.__origin)
         if not deg:
             origin[:2] *= np.pi / 180.
@@ -69,6 +113,24 @@ class CoordinateSystem:
             origin,
             deg=True
     ):
+        """
+        Transform ENU (east, north, up) coordinates from a different coordinate system
+        into this one.
+
+        :param easting: float
+            East, relative to the other coordinate system's origin
+        :param northing: float
+            North, relative to the other coordinate system's origin
+        :param height: float
+            Up, relative to the other coordinate system's origin
+        :param origin: Array of floats with shape (3,)
+            latitude, longitue and elevation of the other coordinate system's origin
+        :param deg: boolean
+            Specifies if the other coordinate system's origin is given in degrees or radians
+        :return: Array of floats with shape (3,)
+            East, north and up coordinates in this coordinate system
+        """
+
         lon, lat, h = pymap3d.enu.enu2geodetic(
             easting,
             northing,
@@ -93,13 +155,18 @@ class CoordinateSystem:
             deg
         )
 
-
-
-
     def get_origin(
             self,
             deg=True
     ):
+        """
+        Get the origin of this coordinate system
+
+        :param deg: boolean
+            If True, latitude and longitude are given in degrees, otherwise in radians
+        :return: Array of floats with shape (3,)
+            Latitude, Longitude and elevation of this coordinate system's origin.
+        """
         if deg:
             return self.__origin
         else:
