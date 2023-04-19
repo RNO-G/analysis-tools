b4c696c1ba5f53e3a5e1e69ddddc19d2a0358c43
Steffen Hallmann <steffen.hallmann@desy.de>
2023-04-19 22:13:08 +0200

    add a simple setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name="rnog_analysis_tools",
+    version='0.1',
+    description='RNO-G Analysis Tools',
+    url='https://github.com/RNO-G/analysis-tools',
+    author='The RNO-G Collaboration',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False)
