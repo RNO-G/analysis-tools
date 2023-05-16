694ecb5b60acffbcd215f8d080c243ed69c6b32b
Steffen Hallmann <steffen.hallmann@desy.de>
2023-05-16 13:54:25 +0200

    add run list identified by Christoph

--- rnog_analysis_tools/glitch_unscrambler/glitch_unscrambler.py
+++ rnog_analysis_tools/glitch_unscrambler/glitch_unscrambler.py
@@ -1,4 +1,5 @@
 def unscramble(trace):
+    """ script to fix scrambled traces (Note: first and last 64 samples are unusable and hence masked with zeros) """
     new_trace = np.zeros_like(trace)
     for i_section in range(len(trace) // 64):
         section_start = i_section * 64
