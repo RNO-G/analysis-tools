b1c7403f78342ec990e7a144a75e1079cfaf5c57
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-08-28 13:02:43 +0200

    Absolute import

--- rnog_analysis_tools/glitch_unscrambler/glitch_detection_per_event.py
+++ rnog_analysis_tools/glitch_unscrambler/glitch_detection_per_event.py
@@ -1,11 +1,13 @@
-from glitch_unscrambler import unscramble
+from rnog_analysis_tools.glitch_unscrambler.glitch_unscrambler import unscramble
+import numpy as np
+
 
 def diff_sq(eventdata):
     """
     Returns sum of squared differences of samples across seams of 128-sample chunks.
 
     `eventdata`: channel waveform
-    """    
+    """
     runsum = 0.0
     deltaN = 64
     for chunk in range(len(eventdata) // 128 - 1):
@@ -22,6 +24,6 @@ def is_channel_scrambled(eventdata):
     To test whether a single event contains glitches, pick a value T0 and evaluate T.
     If T > T0, the event should be classified as containing glitches.
     T0 can be set to control the true-positive and false-positive rates.
-    """    
+    """
     eventdata_us = unscramble(eventdata)
     return diff_sq(eventdata) - diff_sq(eventdata_us)
