f647ffd639ce5b646f6316a8a91cf9ff8d6f795f
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-05-07 12:59:15 +0200

    Add neat function to plot residuals

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -120,3 +120,81 @@ def add_histogram_on_axis(fig, ax, data, n_bins=50, fraction=0.2, which="x", his
     ax2.set_axis_off()
 
     return ax2, hist, edges, patches
+
+
+def draw_residual(ax, ax2, grid=True, relative=True, symetric_limits=True, label="residual"):
+    """ Draws a residual for data of a given axis (ax) on a second axis (ax2).
+
+    It uses the data in `ax.lines` and `ax.collections` to draw the residual.
+    The residual is calculated relative to the first line of the original axis.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The original axis with the data to calculate the residual from.
+    ax2 : matplotlib.axes.Axes
+        The axis to draw the residual on.
+    grid : bool
+        Whether to show a grid on the residual axis.
+    relative : bool
+        Whether to draw a relative (data / data_ref) or absolute residual (data - data_ref).
+    symetric_limits : bool
+        Whether to make the limits of the residual axis symmetric around 0 (absolute residual) or 1 (relative residual).
+    """
+    try:
+        line0 = ax.lines[0]
+        color = line0.get_color()
+        x_ref = line0.get_xdata()
+        y_ref = line0.get_ydata()
+        is_line = True
+    except IndexError:
+        patch0 = ax.patches[0]
+        color = patch0.get_edgecolor()
+        x_ref, y_ref = patch0.get_path().vertices.T
+        is_line = False
+
+
+    if relative:
+        ax2.axhline(1, color=color, lw=2, ls="-")
+    else:
+        ax2.axhline(0, color=color, lw=2, ls="-")
+
+    for poly in ax.collections:
+        x, y = poly.get_paths()[0].vertices.T
+        ref_y_data = np.squeeze([y_ref[x_ref == x_val] for x_val in x])
+
+        try:
+            if relative:
+                ax2.plot(x, y / ref_y_data, alpha=0.5, color=poly.get_facecolor())
+            else:
+                ax2.plot(x, y - ref_y_data, alpha=0.5, color=poly.get_facecolor())
+        except ValueError:
+            pass
+
+    try:
+        for idx, line in enumerate(ax.lines[1:]):
+            assert np.allclose(x_ref, line.get_xdata())
+            if relative:
+                ax2.plot(line.get_xdata(), line.get_ydata() / y_ref, lw=1, color=line.get_color())
+            else:
+                ax2.plot(line.get_xdata(), line.get_ydata() - y_ref, lw=1, color=line.get_color())
+    except ValueError:
+        pass
+
+    for idx, patch in enumerate(ax.patches[1:]):
+        x, y = patch.get_path().vertices.T
+        assert np.allclose(x_ref, x)
+
+        if relative:
+            ax2.plot(x, y / y_ref, lw=1, ls=patch.get_ls(), color=patch.get_edgecolor())
+        else:
+            ax2.plot(x, y - y_ref, lw=1, ls=patch.get_ls(), color=patch.get_edgecolor())
+
+    ax2.grid(grid)
+    ax2.set_ylabel(label)
+
+
+    if symetric_limits:
+        # make symmetric limits around 0 (absolute residual) or 1 (relative residual)
+        lim = np.abs(np.array(ax2.get_ylim()) - int(relative)).max()
+        ax2.set_ylim(-lim + int(relative), lim + int(relative))
