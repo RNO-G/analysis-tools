7c3f45d4f16ae48026b16c9b9bdb4b5037dcdc58
Zeynep Su Selcuk <zeynepsu@wgs52.zeuthen.desy.de>
2025-07-21 19:02:45 +0200

    added plotting function for unnormalized surface channels for st14 config.

--- rnog_analysis_tools/data_monitoring/spectral_analysis.py
+++ rnog_analysis_tools/data_monitoring/spectral_analysis.py
@@ -12,19 +12,25 @@ import pandas as pd
 from argparse import ArgumentParser
 import datetime
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+from matplotlib.lines import Line2D
 
 from NuRadioReco.modules.io.RNO_G.readRNOGDataMattak import readRNOGData
 import rnog_data.runtable as rt
 from NuRadioReco.utilities import units
 
-def reconstruct_data(station_id, channels, overwrite_sampling_rate=2.4, bad_runs_file=None):
+def reconstruct_data(station_id, channels, overwrite_sampling_rate=2.4, start_time="", stop_time="",bad_runs_file=None):
+    '''
+    Obtain the FFT of the force triggered RNO-G data for a specific station and channels, as well as the trace array and frequencies.
+    '''
 
     print(f"Station: {station_id} ; Channels: {channels}")
 
     # Use the RunTable tool to get the runs for a specific station and time range
     RunTable = rt.RunTable()
-    testrt = RunTable.get_table(start_time="2024-06-27T00:00:00", 
-                                stop_time="2024-08-02T00:00:00", 
+    
+    testrt = RunTable.get_table(start_time=start_time, 
+                                stop_time=stop_time, 
                                 stations=[station_id], 
                                 run_types=['physics']
                                 )
@@ -89,11 +95,157 @@ def reconstruct_data(station_id, channels, overwrite_sampling_rate=2.4, bad_runs
         "times": times,
         "event_ids": event_ids,
         "freqs": freqs,
-        "channels": channels
+        "channels": channels,
+        "start_time": start_time,
+        "stop_time": stop_time,
 
     }
 
     return result
+
+
+def plot_time_averaged_spectrum_surface_channels_unnormalized(result, station_id):
+    """
+    Plot the time-averaged spectrum for surface channels.
+    """
+    channels = result['channels']
+    spec_arr = result['spec_arr'] #(n_channels, n_events, n_samples)
+    freqs = result['freqs']
+    start_time = result['start_time']
+    stop_time = result['stop_time']
+    start_date = start_time.split('T')[0]
+    stop_date = stop_time.split('T')[0]
+
+    avg_spectra = spec_arr.mean(axis=1) #(n_channels, n_samples)
+
+    # Style setup
+    mpl.rcParams.update({
+        'font.family': 'sans-serif',
+        'font.sans-serif': ['Helvetica', 'DejaVu Sans', 'Arial'],
+        'font.size': 17,
+        'axes.labelsize': 17,
+        'axes.titlesize': 18,
+        'legend.fontsize': 14,
+        'xtick.labelsize': 17,
+        'ytick.labelsize': 17,
+        'lines.linewidth': 1.3,
+        'lines.antialiased': True
+    })
+
+    plt.figure(figsize=(10, 6))
+
+    # Define known background regions
+    excesscolor = 'grey'
+    plt.axvspan(0.08, 0.12, color=excesscolor, alpha=0.3, label='Excess')
+
+    wb_color = "mediumvioletred"
+    plt.axvline(x=0.402, color=wb_color, linestyle='--', linewidth=1.2, label='Weather Balloon')
+
+    other_bkg = "mediumseagreen"
+    plt.axvspan(0.278, 0.285, color=other_bkg, alpha=0.3, label='periodic signal')
+    plt.axvspan(0.482, 0.485, color=other_bkg, alpha=0.3, label='periodic signal')
+    plt.axvspan(0.240, 0.272, color=other_bkg, alpha=0.3, label='periodic signal')
+    plt.axvspan(0.358, 0.378, color=other_bkg, alpha=0.3, label='periodic signal')
+    plt.axvspan(0.136, 0.139, color=other_bkg, alpha=0.3, label='Walkie Talkie')
+    plt.axvspan(0.151, 0.157, color=other_bkg, alpha=0.3, label='Walkie Talkie')
+    plt.axvspan(0.125, 0.127, color=other_bkg, alpha=0.3, label='Aircraft')
+
+    region_legend_lines = [
+            Line2D([0], [0], color=excesscolor, lw=6, alpha=0.3, label='Excess Region'),
+            Line2D([0], [0], color=wb_color, linestyle='--', label='Weather Balloon'),
+            Line2D([0], [0], color=other_bkg, lw=6, alpha=0.3, label='Other Background')]
+    legend_regions = plt.legend(handles=region_legend_lines,
+                                loc='upper left', 
+                                bbox_to_anchor=(0.818, 0.65), 
+                                frameon=True)
+    plt.gca().add_artist(legend_regions)
+
+    # Station 14 has a different surface channel configuration with 4 up and 4 down channels.
+    if station_id == 14:
+        # Define surface channels for station 14
+        surface_channels = [12,13,14,15,16,17,18,19]
+        # Define up and down channels for station 14
+        up_channels = [13, 15, 16, 18]
+        down_channels = [12, 14, 17, 19]
+
+        # Define colors for each channel (oranges for down, blues for up)
+        colors = {
+            12: "#e6550d",
+            13: "#1f77b4",
+            14: "#f16913",
+            15: "#4292c6",
+            16: "#6baed6",
+            17: "#fd8d3c",
+            18: "#9ecae1",
+            19: "#fdae6b",
+        }
+
+        # Plot the average spectrum for each channel
+        for i, ch in enumerate(channels):
+            if ch in surface_channels:
+                label = f"Ch {ch}"
+                plt.plot(freqs, avg_spectra[i], color=colors[ch], alpha=0.8, label=label)
+
+                # Up and Down channel legends (only those present)
+                handles_down = [Line2D([0], [0], color=colors[ch], lw=2) for ch in down_channels if ch in channels]
+                labels_down = [f"Ch {ch}" for ch in down_channels if ch in channels]
+
+                handles_up = [Line2D([0], [0], color=colors[ch], lw=2) for ch in up_channels if ch in channels]
+                labels_up = [f"Ch {ch}" for ch in up_channels if ch in channels]
+
+                legend_channels = plt.legend(handles=handles_up + handles_down,
+                                            labels=labels_up + labels_down,
+                                            title="   Upward             Downward",
+                                            ncol=2, 
+                                            loc='upper right', 
+                                            frameon=True, 
+                                            columnspacing=1.5)
+                plt.gca().add_artist(legend_channels)
+
+                plt.text(0.02, 0.96,f'St {station_id} | {start_date} to {stop_date}',
+                        transform=plt.gca().transAxes,fontsize=14,
+                        verticalalignment='top', horizontalalignment='left',
+                        bbox=dict(facecolor='white',alpha=0.8, edgecolor='lightgray'))
+
+                plt.xlabel('Frequency [GHz]')
+                plt.ylabel('Time-Averaged Spectrum [V]')
+                plt.grid(True, which="both", axis="both", linestyle=':', linewidth=0.3, alpha=0.7)
+                #plt.xlim(0, 0.8)
+                plt.tight_layout()
+                plt.show()
+
+            else:
+                raise ValueError(f"Channel {ch} is not a surface channel for station {station_id}. Please check the channel list.")
+
+                
+
+                
+
+                
+                
+                
+
+                
+
+
+
+
+
+
+
+            else:
+                
+
+
+
+
+
+    
+
+    
+
+
+    
 
 
 
@@ -107,10 +259,16 @@ if __name__ == "__main__":
     parser.add_argument("--station", type=int, default=14)
     parser.add_argument("--channels", type=int, nargs="+", default=list(range(24)))
     parser.add_argument("--overwrite_sampling_rate", type=float, default=2.4)  # Default to 2.4 GHz
-    parser.add_argument("--bad_runs_file", type=str, default="bad_runs.csv")
+    parser.add_argument("--bad_runs_file", type=str, default=None)
+    parser.add_argument("--start_time", type=str, default="2024-06-27T00:00:00")
+    parser.add_argument("--stop_time", type=str, default="2024-08-02T00:00:00")
     args = parser.parse_args()
 
     print(f"Station: {args.station} ; Channels: {args.channels}")
     
-    reconstruct_data(station_id=args.station, channels=args.channels, bad_runs_file=args.bad_runs_file)
+    results = reconstruct_data(station_id=args.station, 
+                    channels=args.channels,
+                    start_time=args.start_time,
+                    stop_time=args.stop_time,
+                    bad_runs_file=args.bad_runs_file)
 
