add sign test for spectra - forgot to slect FORCE trigger data, fix it

rnog_analysis_tools/data_monitoring/science_verification_analysis.py
@@ -32,7 +32,10 @@ This module can be used to test if the stations are working as expected.
 SURFACE_CHANNELS_LIST = [12, 13, 14, 15, 16, 17, 18 , 19 , 20]
 DEEP_CHANNELS_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 21, 22, 23]
 UPWARD_CHANNELS_LIST = [13, 16, 19]
-DOWNWARD_CHANNELS_LIST = [12, 14, 15, 17, 18]
+DOWNWARD_CHANNELS_LIST = [12, 14, 15, 17, 18, 20]
+VPOL_LIST = [0, 1, 2, 3, 5, 6, 7, 9, 10, 22, 23]
+HPOL_LIST = [4, 8, 11, 21]
+PHASED_ARRAY_LIST = [0, 1, 2, 3]
 ALL_CHANNELS_LIST = SURFACE_CHANNELS_LIST + DEEP_CHANNELS_LIST
 
 # Channel mapping for Station 14:
@@ -40,6 +43,9 @@ STATION_14_SURFACE_CHANNELS_LIST = [12, 13, 14, 15, 16, 17, 18 , 19]
 STATION_14_DEEP_CHANNELS_LIST = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 22, 23]
 STATION_14_UPWARD_CHANNELS_LIST = [13, 15, 16, 18]
 STATION_14_DOWNWARD_CHANNELS_LIST = [12, 14, 17, 19]
+STATION_14_VPOL_LIST = [0, 1, 2, 3, 5, 6, 7, 9, 10, 20, 22, 23]
+STATION_14_HPOL_LIST = [4, 8, 11, 21]
+STATION_14_PHASED_ARRAY_LIST = [0, 1, 2, 3]
 STATION_14_ALL_CHANNELS_LIST = STATION_14_SURFACE_CHANNELS_LIST + STATION_14_DEEP_CHANNELS_LIST
 
 # Matplotlib settings
@@ -110,13 +116,13 @@ def convert_events_information(event_info, convert_to_arrays=True):
 # Find the correct channel mapping for a given station
 def station_channels(station_id: int):
     if station_id == 14:
-        return STATION_14_SURFACE_CHANNELS_LIST, STATION_14_DEEP_CHANNELS_LIST, STATION_14_UPWARD_CHANNELS_LIST, STATION_14_DOWNWARD_CHANNELS_LIST, STATION_14_ALL_CHANNELS_LIST
+        return STATION_14_SURFACE_CHANNELS_LIST, STATION_14_DEEP_CHANNELS_LIST, STATION_14_UPWARD_CHANNELS_LIST, STATION_14_DOWNWARD_CHANNELS_LIST, STATION_14_VPOL_LIST, STATION_14_HPOL_LIST, STATION_14_PHASED_ARRAY_LIST, STATION_14_ALL_CHANNELS_LIST
     else:
-        return SURFACE_CHANNELS_LIST, DEEP_CHANNELS_LIST, UPWARD_CHANNELS_LIST, DOWNWARD_CHANNELS_LIST, ALL_CHANNELS_LIST
+        return SURFACE_CHANNELS_LIST, DEEP_CHANNELS_LIST, UPWARD_CHANNELS_LIST, DOWNWARD_CHANNELS_LIST, VPOL_LIST, HPOL_LIST, PHASED_ARRAY_LIST, ALL_CHANNELS_LIST
 
 # Normalize surface channel spectra to the average of the down channels in the reference frequency band (500-650 MHz)
-def normalize_surface_channels_to_down_reference(spec_arr, frequencies, down_channels, up_channels, f_low=500*units.MHz, f_high=650*units.MHz):
-    '''Normalize all surface channel spectra to the average of the down channels in the reference frequency band.'''
+def normalize_channels(spec_arr, frequencies, down_channels, up_channels, f_low=500*units.MHz, f_high=650*units.MHz):
+    '''Normalize all surface channel spectra to the average of the down channels in the reference frequency band. The reference band is 500-650 MHz by default. This will be replaced by lab measurements in the future.'''
     # spec_arr shape: (n_channels, n_events, n_freqs)
     freq_mask = (frequencies >= f_low) & (frequencies <= f_high)
     spec_arr = np.copy(spec_arr)
@@ -145,7 +151,23 @@ def read_rnog_runtable(station_id: int, start_time: str, stop_time: str):
 
 def read_rnog_data(station_id: int, run_numbers: list, backend: str = "pyroot"):
     '''Read RNO-G data for a given station and list of run numbers using the specified backend.'''
-    file_list = [ f"/pnfs/ifh.de/acs/radio/diskonly/data/inbox/station14/run{run_id}/combined.root" for run_id in run_numbers]
+    file_list = []
+    valid_run_numbers = []
+    missing_runs = []
+
+    for run_id in run_numbers:
+        path = f"/pnfs/ifh.de/acs/radio/diskonly/data/inbox/station{station_id}/run{run_id}/combined.root"
+        if os.path.isfile(path):
+            file_list.append(path)
+            valid_run_numbers.append(run_id)
+        else:
+            missing_runs.append(run_id)
+
+    if missing_runs:
+        print(f"!!!! Skipping {len(missing_runs)} missing runs: {missing_runs} !!!!")
+    if not file_list:
+        raise FileNotFoundError("No combined.root files found for selected runs.")
+    
     n_files = len(file_list)
     n_batches = n_files // 100 + 1
     print(f"Reading {n_files} files in {n_batches} batches using {backend} backend.")
@@ -162,6 +184,8 @@ def read_rnog_data(station_id: int, run_numbers: list, backend: str = "pyroot"):
     freqs = None
 
     from NuRadioReco.modules.channelSignalReconstructor import channelSignalReconstructor as csr
+    csr = csr()
+    csr.begin(debug=False)
 
     for batch in tqdm(np.array_split(np.array(file_list), n_batches), desc="Reading batches", unit="batch"):
         tableReader = dataProviderRNOG()
@@ -190,9 +214,6 @@ def read_rnog_data(station_id: int, run_numbers: list, backend: str = "pyroot"):
         run_no = []
         times = []
         event_ids = []
-
-        csr = csr()
-        csr.begin(debug=False)
         
         for idx, event in enumerate(tqdm(tableReader.reader.run(), total=n_events, desc="Events", unit="evt", leave=False)):
             station = event.get_station()
@@ -249,7 +270,117 @@ def read_rnog_data(station_id: int, run_numbers: list, backend: str = "pyroot"):
     #print(f"freqs {freqs}")
     #print(f"trigger types: {np.unique(event_info['triggerType'])}")
 
-def plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freqs, upward_channels, downward_channels, save_location):
+def find_amplitude_ratio_in_band(station_id, freqs, norm_spec_arr, upward_channels, downward_channels, reference_channels, freq_min, freq_max):
+    '''Find normalized amplitude ratio of upward vs downward channels in a given frequency band. This will be replaced by lab measurements in the future.'''
+    freq_mask = (freqs >= freq_min) & (freqs <= freq_max)
+    ratio_list = []
+    spectrum_list = []
+
+    ref_specs = np.stack([norm_spec_arr[ch][:, freq_mask] for ch in reference_channels], axis=0)
+    ref_spectra_per_ch = np.median(ref_specs, axis=2)   # (n_ref_ch, n_events)
+    ref_spec = np.median(ref_spectra_per_ch, axis=0) # (n_events,)
+
+    for ch in upward_channels + downward_channels:
+        masked_spec = norm_spec_arr[ch][:, freq_mask]        # (n_events, n_freqs)
+        masked_spec_med = np.median(masked_spec, axis=1)       # (n_events,)
+
+        spec_ratio = masked_spec_med / ref_spec
+        ratio_list.append(spec_ratio)
+        spectrum_list.append(masked_spec_med)
+
+    return ratio_list, spectrum_list
+   
+
+def find_amplitude_ratio_in_band_specific_bkg(station_id, freqs, norm_spec_arr, upward_channels, downward_channels):
+    '''Find normalized amplitude ratio of upward vs downward channels in specific frequency bands. Backgrouns were defined using wiki page: https://radio.uchicago.edu/wiki/index.php/Features_observed_in_data'''
+
+    # Galactic excess frequency band
+    if station_id == 14:
+        reference_channels_gal = [12, 14, 19]
+    else:
+        reference_channels_gal = downward_channels
+    freq_min_gal = 80 * units.MHz
+    freq_max_gal = 120 * units.MHz
+    ratio_arr_gal, _ = find_amplitude_ratio_in_band(station_id, freqs, norm_spec_arr, upward_channels, downward_channels, reference_channels_gal, freq_min_gal, freq_max_gal)
+
+    # 360-380 MHz frequency band
+    reference_channels_360 = downward_channels
+    freq_min_360 = 360 * units.MHz
+    freq_max_360 = 380 * units.MHz
+    ratio_arr_360, _ = find_amplitude_ratio_in_band(station_id, freqs, norm_spec_arr, upward_channels, downward_channels, reference_channels_360, freq_min_360, freq_max_360)
+
+    # 482-485 MHz frequency band
+    reference_channels_482 = downward_channels
+    freq_min_482 = 482 * units.MHz
+    freq_max_485 = 485 * units.MHz
+    ratio_arr_482, _ = find_amplitude_ratio_in_band(station_id, freqs, norm_spec_arr, upward_channels, downward_channels, reference_channels_482, freq_min_482, freq_max_485)
+
+    return ratio_arr_gal, ratio_arr_360, ratio_arr_482
+
+def excess_info_from_ratio(ratio_arr, alpha=0.01):
+    '''Calculate excess information from amplitude ratios in frequency bands.'''
+    from scipy.stats import binomtest
+
+    log_ratio = np.log10(np.asarray(ratio_arr))
+    median_log_ratio = np.median(log_ratio)
+    mean_log_ratio = np.mean(log_ratio)
+    frac_pos = np.mean(log_ratio > 0)/np.mean(log_ratio < 0) if np.mean(log_ratio < 0) != 0 else np.inf
+
+    k = np.sum(log_ratio > 0)
+    n = int(log_ratio.size)
+    pval = binomtest(k, n, p=0.5, alternative="greater").pvalue
+    print(f"Binomial test: k={k}, n={n}, pval={pval:.3e}")
+
+    if pval < alpha:
+        validation = "EXCESS (sign-test)"
+    else:
+        validation = "NO EXCESS (sign-test)"
+
+    return {
+        "median_log_ratio": median_log_ratio,
+        "mean_log_ratio": mean_log_ratio,
+        "frac_pos": frac_pos,
+        "pval": pval,
+        "validation": validation
+    }
+
+def validate_excess_in_bands(ratio_arr_gal, ratio_arr_360, ratio_arr_482, alpha=0.01):
+    '''Validate excess information from amplitude ratios in frequency bands.'''
+    gal_info = excess_info_from_ratio(ratio_arr_gal, alpha)
+    freq360_info = excess_info_from_ratio(ratio_arr_360, alpha)
+    freq482_info = excess_info_from_ratio(ratio_arr_482, alpha)
+
+    return {
+        "galactic_excess": gal_info,
+        "freq_360_380MHz": freq360_info,
+        "freq_482_485MHz": freq482_info
+    }
+
+def debug_plot_ratios(ratio_arr_gal, ratio_arr_360, ratio_arr_482, channels_order, save_location, station_id, run_label, bins=30):
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
+    bands = [("Galactic Excess (80–120 MHz)", ratio_arr_gal),
+             ("360–380 MHz", ratio_arr_360),
+             ("482–485 MHz", ratio_arr_482),]
+
+    for ax, (title, ratio_list) in zip(axes, bands):
+        for ch, r in zip(channels_order, ratio_list):
+            r = np.asarray(r)
+            ax.hist(np.log10(r), bins=bins, histtype="step", linewidth=1.3, label=f"Ch {ch}", alpha=0.8)
+
+        ax.set_title(title)
+        ax.set_xlabel("log10 Amplitude Ratio")
+        ax.grid(True, alpha=0.3)
+
+    axes[0].set_ylabel("Counts")
+
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="upper right", ncol=8, frameon=True)
+    plt.tight_layout()
+    fig.savefig(os.path.join(save_location,f"debug_amplitude_ratios_{station_id}_{run_label}.pdf",))
+    plt.close(fig)
+
+
+def plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freqs, upward_channels, downward_channels, save_location, run_label):
     '''Plot time-integrated surface channel spectra.'''
     plt.figure(figsize=(10, 6))
     for ch in upward_channels:
@@ -261,7 +392,7 @@ def plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freq
 
     plt.xlabel('Frequency [MHz]')
     plt.xlim(0, 800)
-    plt.ylabel('Mean Amplitude Spectrum [V/GHz]')
+    plt.ylabel('Amplitude Spectrum [V/GHz]')
     plt.title('Time-Integrated Spectrum of Surface Channels')
     plt.legend(loc="upper right", 
                frameon=True,
@@ -270,10 +401,10 @@ def plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freq
                edgecolor="black")
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(save_location, f"time_integrated_surface_spectra_unnormalized_{station_id}.pdf"))
+    plt.savefig(os.path.join(save_location, f"time_integrated_surface_spectra_unnormalized_{station_id}_{run_label}.pdf"))
 
 
-def plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, freqs, upward_channels, downward_channels, save_location):
+def plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, freqs, upward_channels, downward_channels, save_location, run_label):
     '''Plot time-integrated surface channel spectra.'''
     plt.figure(figsize=(10, 6))
     for ch in upward_channels:
@@ -301,7 +432,7 @@ def plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, f
 
     plt.xlabel('Frequency [MHz]')
     plt.xlim(0, 800)
-    plt.ylabel('Mean Amplitude Spectrum [V/GHz]')
+    plt.ylabel('Amplitude Spectrum [V/GHz]')
     plt.title('Time-Integrated Spectrum of Surface Channels')
     
     ax = plt.gca()
@@ -330,7 +461,30 @@ def plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, f
 
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(save_location, f"time_integrated_surface_spectra_normalized_{station_id}.pdf"))
+    plt.savefig(os.path.join(save_location, f"time_integrated_surface_spectra_normalized_{station_id}_{run_label}.pdf"))
+
+def plot_time_integrated_deep_spectra(station_id, spec_arr, freqs, vpol_channels, hpol_channels, save_location, run_label):
+    '''Plot time-integrated deep channel spectra.'''
+    plt.figure(figsize=(10, 6))
+    for ch in vpol_channels:
+        spec_mean = np.mean(spec_arr[ch, :, :], axis=0)
+        plt.plot(freqs / units.MHz, spec_mean, label=f'Ch {ch} (VPOL)', linestyle='-')
+    for ch in hpol_channels:
+        spec_mean = np.mean(spec_arr[ch, :, :], axis=0)
+        plt.plot(freqs / units.MHz, spec_mean, label=f'Ch {ch} (HPOL)', linestyle='--')
+
+    plt.xlabel('Frequency [MHz]')
+    plt.xlim(0, 800)
+    plt.ylabel('Amplitude Spectrum [V/GHz]')
+    plt.title('Time-Integrated Spectrum of Deep Channels')
+    plt.legend(loc="upper right", 
+               frameon=True,
+               fancybox=True,
+               framealpha=0.9,
+               edgecolor="black")
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(os.path.join(save_location, f"time_integrated_deep_spectra_unnormalized_{station_id}_{run_label}.pdf"))
 
 if __name__ == "__main__":
 
@@ -338,7 +492,9 @@ if __name__ == "__main__":
     argparser.add_argument("-st", "--station_id", type=int, required=True, help="Station to analyze, e.g --station_id 14")
     argparser.add_argument("-b", "--backend", type=str, default="pyroot", help="Backend to use for reading data, should be either pyroot or uproot (default: pyroot), e.g. --backend pyroot or --backend uproot")
     argparser.add_argument("-sl", "--save_location", type=str, default=".", help="Location to save the output plots (default: current directory), e.g. --save_location /path/to/save/plots")
-    
+    argparser.add_argument("-ex", "--exclude-runs", nargs="+", type=int, default=[], metavar="RUN", help="Run number(s) to exclude, e.g. --exclude-runs 1005 1010")
+    argparser.add_argument("--debug_plot", action="store_true", help="If set, will create debug plots for amplitude ratios in frequency bands.")
+
     run_selection = argparser.add_mutually_exclusive_group(required=True)
     run_selection.add_argument("--runs", nargs="+", type=int, metavar="RUN_NUMBERS",
                            help="Run number(s) to analyze. Each run number should be given explicitly separated by a space, e.g. --runs 1001 1002 1005")
@@ -348,7 +504,7 @@ if __name__ == "__main__":
                             help="Date range to analyze (inclusive). Provide start and end dates separated by a space in YYYY-MM-DD format, e.g. --time_range 2024-07-15 2024-09-30")
 
 
-    surface_channels, deep_channels, upward_channels, downward_channels, all_channels = station_channels(argparser.parse_args().station_id)
+    surface_channels, deep_channels, upward_channels, downward_channels, vpol_channels, hpol_channels, phased_array_channels, all_channels = station_channels(argparser.parse_args().station_id)
 
     args = argparser.parse_args()
 
@@ -362,17 +518,59 @@ if __name__ == "__main__":
     elif args.run_range:
         run_numbers = list(range(args.run_range[0], args.run_range[1] + 1))
     elif args.time_range:
-        start_time = args.time_range[0]
-        stop_time = args.time_range[1]
+        start_time, stop_time = args.time_range
         runtable = read_rnog_runtable(station_id, start_time, stop_time)
-        run_numbers = runtable['run'].tolist()
+        run_numbers = runtable["run"].tolist()
+    else:
+        raise ValueError("No run selection provided")
+    
+    # Exclude specified runs
+    if args.exclude_runs:
+        exclude_set = set(args.exclude_runs)
+        run_numbers = [r for r in run_numbers if r not in exclude_set]
+
+    run_numbers = sorted(run_numbers)
+    first_run = run_numbers[0]
+    last_run = run_numbers[-1]
+
+    if first_run == last_run:
+        run_label = f"run_{first_run}"
+    else:
+        run_label = f"runs_{first_run}_{last_run}"
+
 
     # Create save location directory if it doesn't exist
     save_location = os.path.expanduser(args.save_location)
     os.makedirs(save_location, exist_ok=True)
 
     spec_arr, trace_arr, times_trace_arr, snr_arr, run_no, times, freqs, event_info = read_rnog_data(station_id, run_numbers, backend=backend) 
-    norm_spec_arr, scale_factors = normalize_surface_channels_to_down_reference(spec_arr, freqs, downward_channels, upward_channels)
+    norm_spec_arr, scale_factors = normalize_channels(spec_arr, freqs, downward_channels, upward_channels)
 
-    plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freqs, upward_channels, downward_channels, save_location)
-    plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, freqs, upward_channels, downward_channels, save_location)
+    ratio_arr_gal, ratio_arr_360, ratio_arr_482 = find_amplitude_ratio_in_band_specific_bkg(station_id, freqs, norm_spec_arr, upward_channels, downward_channels)
+    channels_order = upward_channels + downward_channels
+    ch_to_idx = {ch: i for i, ch in enumerate(channels_order)}
+    print(ch_to_idx)
+
+    for ch in surface_channels:
+        print(f"\nAnalyzing channel {ch}")
+        i = ch_to_idx[ch]
+        print(i)
+
+        validation_results = validate_excess_in_bands(
+            ratio_arr_gal[i],
+            ratio_arr_360[i],
+            ratio_arr_482[i],
+            alpha=0.01
+        )
+
+        for band, results in validation_results.items():
+            print(f"=== {band} ===")
+            for key, value in results.items():
+                print(f"{key}: {value}")
+            print("==============")
+    
+    plot_time_integrated_surface_spectra_unnormalized(station_id, spec_arr, freqs, upward_channels, downward_channels, save_location, run_label)
+    plot_time_integrated_surface_spectra_normalized(station_id, norm_spec_arr, freqs, upward_channels, downward_channels, save_location, run_label)
+
+    if args.debug_plot:   
+        debug_plot_ratios(ratio_arr_gal, ratio_arr_360, ratio_arr_482, channels_order=channels_order, save_location=save_location, station_id=station_id, run_label=run_label, bins=30,)